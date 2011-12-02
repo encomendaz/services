3434d40dc1ac1715db732e89c4d1b7f40f0f505e
Cleverson Sacramento <cleverson.sacramento@gmail.com>
2011-12-02 09:11:24 -0300

    Arquivos restantes do último commit

---
+++ src/main/java/net/encomendaz/rest/Configuration.java
@@ -0,0 +1,69 @@
+/*
+ * EncomendaZ
+ * 
+ * Copyright (c) 2011, EncomendaZ <http://encomendaz.net>.
+ * All rights reserved.
+ * 
+ * EncomendaZ is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 3 of the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see <http://gnu.org/licenses>
+ * or write to the Free Software Foundation, Inc., 51 Franklin Street,
+ * Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package net.encomendaz.rest;
+
+import java.util.ResourceBundle;
+
+public class Configuration {
+
+	private String awsAccessKey;
+
+	private String awsSecretKey;
+
+	private ResourceBundle resourceBundle;
+
+	private static Configuration instance;
+
+	private Configuration() {
+	}
+
+	public static Configuration getInstance() {
+		if (instance == null) {
+			instance = new Configuration();
+		}
+
+		return instance;
+	}
+
+	public String getAwsAccessKey() {
+		if (awsAccessKey == null) {
+			awsAccessKey = getResourceBundle().getString("aws.access.key");
+		}
+
+		return awsAccessKey;
+	}
+
+	public String getAwsSecretKey() {
+		if (awsSecretKey == null) {
+			awsSecretKey = getResourceBundle().getString("aws.secret.key");
+		}
+
+		return awsSecretKey;
+	}
+
+	private ResourceBundle getResourceBundle() {
+		if (resourceBundle == null) {
+			resourceBundle = ResourceBundle.getBundle("META-INF/configuration");
+		}
+
+		return resourceBundle;
+	}
+}
---
+++ src/test/java/net/encomendaz/rest/ConfigurationTest.java
@@ -0,0 +1,18 @@
+package net.encomendaz.rest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class ConfigurationTest {
+	
+	
+	private Configuration configuration = Configuration.getInstance();
+
+	@Test
+	public void test() {
+		
+	}
+
+}

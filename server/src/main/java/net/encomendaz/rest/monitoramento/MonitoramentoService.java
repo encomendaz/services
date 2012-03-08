f65e788cdb4f6e9fd8509f4efdcc69696b169a9f
Cleverson Sacramento <cleverson.sacramento@gmail.com>
2012-03-08 15:07:23 -0300

    Desativação temporária do serviço de monitoramento para reestruturação

--- server/src/main/java/net/encomendaz/rest/monitoramento/MonitoramentoService.java
+++ server/src/main/java/net/encomendaz/rest/monitoramento/MonitoramentoService.java
@@ -23,19 +23,15 @@ package net.encomendaz.rest.monitoramento;
 import java.util.List;
 
 import javax.ws.rs.FormParam;
-import javax.ws.rs.GET;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 
 import net.encomendaz.rest.util.Serializer;
 
-@Path("/monitoramento.json")
-@Produces("application/json;charset=UTF-8")
+//@Path("/monitoramento.json")
+//@Produces("application/json;charset=UTF-8")
 public class MonitoramentoService {
 
-	@PUT
+	// @PUT
 	public void cadastrar(@FormParam("id") String id, @FormParam("email") String email) throws AlreadyExistsException {
 		Monitoramento monitoramento = new Monitoramento();
 		monitoramento.setId(id);
@@ -44,7 +40,7 @@ public class MonitoramentoService {
 		MonitoramentoManager.cadastrar(monitoramento);
 	}
 
-	@GET
+	// @GET
 	public String listar(@QueryParam("email") String email, @QueryParam("jsonp") String jsonp) {
 		List<Monitoramento> monitoramentos = MonitoramentoManager.obter(email);
 		return Serializer.json(monitoramentos, jsonp);

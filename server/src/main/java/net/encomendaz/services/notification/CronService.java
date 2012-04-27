c667f5aa916327c82510a48e0af3007f7e070247
Cleverson Sacramento <cleverson.sacramento@gmail.com>
2012-04-27 14:16:46 -0300

    Início dos experimentos para implementação de push notifications

---
+++ server/src/main/java/net/encomendaz/services/notification/CronService.java
@@ -0,0 +1,35 @@
+/*
+ * EncomendaZ
+ * 
+ * Copyright (c) 2011, EncomendaZ <http://encomendaz.net>.
+ * All rights reserved.
+ * 
+ * EncomendaZ is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 3 of the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see <http://gnu.org/licenses>
+ * or write to the Free Software Foundation, Inc., 51 Franklin Street,
+ * Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package net.encomendaz.services.notification;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@Produces
+@Path("/notification.cron")
+public class CronService {
+
+	@GET
+	public void x() {
+		System.out.println("xxxx");
+	}
+}

1ea654b20a8c524f22b142d70a23e98ae846eb2c
Cleverson Sacramento <cleverson.sacramento@gmail.com>
2012-10-31 05:33:23 -0300

    Inclusão da dependência ao httpclient

---
+++ server/src/test/java/net/encomendaz/services/notification/NotificationTest.java
@@ -0,0 +1,29 @@
+package net.encomendaz.services.notification;
+
+import org.codehaus.jackson.jaxrs.JacksonJsonProvider;
+import org.jboss.resteasy.client.ClientRequest;
+import org.jboss.resteasy.client.ProxyFactory;
+import org.jboss.resteasy.spi.ResteasyProviderFactory;
+import org.junit.Test;
+
+public class NotificationTest {
+
+	@Test
+	public void send() {
+		ResteasyProviderFactory.setRegisterBuiltinByDefault(false);
+		ResteasyProviderFactory.getInstance().registerProvider(JacksonJsonProvider.class);
+		ClientRequest.setDefaultExecutorClass(AirshipClientExecutor.class.getCanonicalName());
+
+		Aps aps = new Aps();
+		aps.setAlert("Olá!");
+		aps.setSound("default");
+		// aps.setBadge("+1");
+
+		Push push = new Push();
+		push.addAlias("86543654199261380431726829184927");
+		push.setAps(aps);
+
+		NotificationProxy proxy = ProxyFactory.create(NotificationProxy.class, "https://go.urbanairship.com");
+		proxy.notify(push);
+	}
+}

9af6ad379a5446dd29dd1c573282b8f4cb3e7fdf
Cleverson Sacramento <cleverson.sacramento@gmail.com>
2012-11-01 16:36:15 -0300

    Restrição de acesso às URLs privadas

--- server/src/main/backup/AlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package net.encomendaz.services.monitoramento;
-
-public class AlreadyExistsException extends Exception {
-
-	private static final long serialVersionUID = 1L;
-
-}
--- server/src/main/backup/DoesNotExistException.java
+++
@@ -1,7 +0,0 @@
-package net.encomendaz.services.monitoramento;
-
-public class DoesNotExistException extends Exception {
-
-	private static final long serialVersionUID = 1L;
-
-}

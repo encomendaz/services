45239c7038f2a157979547ed6690ae7a933067af
Cleverson Sacramento <cleverson.sacramento@gmail.com>
2012-11-26 18:27:36 -0300

    Cache L2 carregado sob demanda

--- server/src/main/java/net/encomendaz/services/monitoring/MonitoringPersistence.java
+++ server/src/main/java/net/encomendaz/services/monitoring/MonitoringPersistence.java
@@ -20,21 +20,11 @@
  */
 package net.encomendaz.services.monitoring;
 
-import static com.google.appengine.api.taskqueue.TaskOptions.Method.DELETE;
-import static com.google.appengine.api.taskqueue.TaskOptions.Method.POST;
-
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-
-import javax.ws.rs.DELETE;
-import javax.ws.rs.FormParam;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.QueryParam;
 
 import net.encomendaz.services.EncomendaZException;
 import net.encomendaz.services.tracking.Tracking;
@@ -48,15 +38,13 @@ import com.google.appengine.api.datastore.EntityNotFoundException;
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.KeyFactory;
 import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.PropertyProjection;
 import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.FilterPredicate;
 import com.google.appengine.api.memcache.MemcacheService;
+import com.google.appengine.api.memcache.MemcacheService.IdentifiableValue;
 import com.google.appengine.api.memcache.MemcacheServiceFactory;
-import com.google.appengine.api.taskqueue.Queue;
-import com.google.appengine.api.taskqueue.QueueFactory;
-import com.google.appengine.api.taskqueue.TaskOptions;
-import com.google.appengine.api.taskqueue.TaskOptions.Builder;
 
-@Path("/admin/monitoring/cache")
 public class MonitoringPersistence {
 
 	private static DatastoreService getDatastoreService() {
@@ -72,7 +60,6 @@ public class MonitoringPersistence {
 	}
 
 	public static void insert(Monitoring monitoring) throws EncomendaZException {
-		before();
 		Tracking tracking = TrackingManager.search(monitoring.getTrackId());
 
 		if (tracking.isCompleted()) {
@@ -96,9 +83,10 @@ public class MonitoringPersistence {
 			setProperty(entity, "label", monitoring.getLabel());
 
 			getDatastoreService().put(entity);
-			getMemcacheService().put(id, monitoring);
 
-			addToCacheQueue(id);
+			addMonitoringToCache(monitoring);
+			addTrackIdToCache(monitoring.getClientId(), monitoring.getTrackId());
+			addClientIdToCache(monitoring.getClientId());
 		}
 	}
 
@@ -120,8 +108,6 @@ public class MonitoringPersistence {
 	}
 
 	public static void update(Monitoring monitoring) {
-		before();
-
 		String clientId = monitoring.getClientId();
 		String trackId = monitoring.getTrackId();
 		String id = getId(clientId, trackId);
@@ -139,134 +125,205 @@ public class MonitoringPersistence {
 		if (updated) {
 			getDatastoreService().put(entity);
 
-			getMemcacheService().delete(id);
-			getMemcacheService().put(id, monitoring);
+			addMonitoringToCache(monitoring);
+			addTrackIdToCache(monitoring.getClientId(), monitoring.getTrackId());
+			addClientIdToCache(monitoring.getClientId());
 		}
 	}
 
 	public static void delete(Monitoring monitoring) {
-		before();
 		String id = getId(monitoring);
-
 		getDatastoreService().delete(getKey(id));
-		getMemcacheService().delete(id);
 
-		removeFromCacheQueue(id);
+		removeMonitoringFromCache(monitoring);
+		removeTrackIdFromCache(monitoring.getClientId(), monitoring.getTrackId());
 	}
 
-	private static void before() {
-		final String key = "cache_status";
+	private static Set<String> getClientIds() {
+		@SuppressWarnings("unchecked")
+		Set<String> clientIds = (Set<String>) getMemcacheService().get(getKind());
 
-		if (Strings.isEmpty((String) getMemcacheService().get(key))) {
-			getMemcacheService().put(key, "loading");
-			// loadCacheQueue();
-			loadCache();
-		}
+		if (clientIds == null) {
+			Query query = new Query(getKind());
+			query.addProjection(new PropertyProjection("clientId", String.class));
 
-		while (!getMemcacheService().get(key).equals("loaded")) {
-			try {
-				Thread.sleep(5000);
+			PreparedQuery preparedQuery = getDatastoreService().prepare(query);
+			clientIds = new HashSet<String>();
+			String clientId;
 
-			} catch (InterruptedException cause) {
-				throw new RuntimeException(cause);
+			for (Entity entity : preparedQuery.asIterable()) {
+				clientId = getProperty(entity, "clientId");
+
+				addClientIdToCache(clientId);
+				clientIds.add(clientId);
 			}
 		}
+
+		return clientIds;
 	}
 
-	// private static void loadCacheQueue() {
-	// TaskOptions taskOptions;
-	// taskOptions = Builder.withUrl("/admin/monitoring/cache");
-	// taskOptions.method(PUT);
+	private static Set<String> getTrackIds(String clientId) {
+		@SuppressWarnings("unchecked")
+		Set<String> trackIds = (Set<String>) getMemcacheService().get(clientId);
+
+		if (trackIds == null) {
+			Query query = new Query(getKind());
+			query.addProjection(new PropertyProjection("trackId", String.class));
+			query.setFilter(new FilterPredicate("clientId", Query.FilterOperator.EQUAL, clientId));
+
+			PreparedQuery preparedQuery = getDatastoreService().prepare(query);
+			trackIds = new HashSet<String>();
+			String trackId;
+
+			for (Entity entity : preparedQuery.asIterable()) {
+				trackId = getProperty(entity, "trackId");
+
+				addTrackIdToCache(clientId, trackId);
+				trackIds.add(trackId);
+			}
+		}
+
+		return trackIds;
+	}
+
+	private static void addMonitoringToCache(Monitoring monitoring) {
+		String id = getId(monitoring);
+		getMemcacheService().put(id, monitoring);
+
+		// addTrackIdToCache(monitoring.getClientId(), monitoring.getTrackId());
+		// addClientIdToCache(monitoring.getClientId());
+	}
+
+	private static boolean removeMonitoringFromCache(Monitoring monitoring) {
+		String id = getId(monitoring);
+		boolean deleted = getMemcacheService().delete(id);
+
+		if (deleted) {
+			removeTrackIdFromCache(monitoring.getClientId(), monitoring.getTrackId());
+		}
+
+		return deleted;
+	}
+
+	@SuppressWarnings("unchecked")
+	private static boolean addTrackIdToCache(String clientId, String trackId) {
+		boolean result = true;
+
+		IdentifiableValue identifiable = getMemcacheService().getIdentifiable(clientId);
+		Set<String> trackIds;
+
+		if (identifiable != null) {
+			trackIds = (Set<String>) identifiable.getValue();
+			trackIds.add(trackId);
+
+			result = getMemcacheService().putIfUntouched(clientId, identifiable, trackIds);
+
+		} else {
+			trackIds = new HashSet<String>();
+			trackIds.add(trackId);
+
+			getMemcacheService().put(clientId, trackIds);
+		}
+
+		if (!result) {
+			addTrackIdToCache(clientId, trackId);
+		}
+
+		// addClientIdToCache(clientId);
+		return result;
+	}
+
+	@SuppressWarnings("unchecked")
+	private static boolean removeTrackIdFromCache(String clientId, String trackId) {
+		boolean result = true;
+
+		IdentifiableValue identifiable = getMemcacheService().getIdentifiable(clientId);
+		Set<String> trackIds;
+
+		if (identifiable != null) {
+			trackIds = (Set<String>) identifiable.getValue();
+
+			if (trackIds.remove(trackId)) {
+				result = getMemcacheService().putIfUntouched(clientId, identifiable, trackIds);
+			}
+		}
+
+		if (!result) {
+			removeTrackIdFromCache(clientId, trackId);
+		}
+
+		return result;
+	}
+
+	@SuppressWarnings("unchecked")
+	private static boolean addClientIdToCache(String clientId) {
+		boolean result = true;
+
+		IdentifiableValue identifiable = getMemcacheService().getIdentifiable(getKind());
+		Set<String> clientIds;
+
+		if (identifiable != null) {
+			clientIds = (Set<String>) identifiable.getValue();
+			clientIds.add(clientId);
+
+			result = getMemcacheService().putIfUntouched(getKind(), identifiable, clientIds);
+
+		} else {
+			clientIds = new HashSet<String>();
+			clientIds.add(clientId);
+
+			getMemcacheService().put(getKind(), clientIds);
+		}
+
+		if (!result) {
+			addClientIdToCache(clientId);
+		}
+
+		return result;
+	}
+
+	// private static boolean removeClientIdFromCache(String clientId) {
+	// boolean result = true;
 	//
-	// Queue queue = QueueFactory.getQueue("monitoring-cache");
-	// queue.add(taskOptions);
+	// IdentifiableValue identifiable = getMemcacheService().getIdentifiable(getKind());
+	// Set<String> clientIds;
+	//
+	// if (identifiable != null) {
+	// clientIds = (Set<String>) identifiable.getValue();
+	//
+	// if (clientIds.remove(clientId)) {
+	// result = getMemcacheService().putIfUntouched(clientId, identifiable, clientIds);
+	// }
+	// }
+	//
+	// if (!result) {
+	// removeClientIdFromCache(clientId);
+	// }
+	//
+	// return result;
 	// }
 
-	// @PUT
-	private static void loadCache() {
-		for (String id : findAllIdsFromDatastore()) {
-			// addToCache(id);
-			addToCacheQueue(id);
-		}
-
-		addToCacheQueue(null);
-
-		//
-		// final String key = "cache_status";
-		// getMemcacheService().put(key, "loaded");
-	}
-
-	public static Set<String> getClientIds() {
-		before();
-
-		return getCachedClientIds();
-	}
-
-	@SuppressWarnings("unchecked")
-	private static Set<String> getCachedClientIds() {
-		Set<String> result = (Set<String>) getMemcacheService().get(getKind());
-
-		if (result == null) {
-			result = Collections.synchronizedSet(new HashSet<String>());
-		}
-
-		return result;
-	}
-
-	@SuppressWarnings("unchecked")
-	private static Set<String> getCachedTrackIds(String clientId) {
-		Set<String> result = null;
-
-		if (!Strings.isEmpty(clientId)) {
-			result = (Set<String>) getMemcacheService().get(clientId);
-		}
-
-		if (result == null) {
-			result = Collections.synchronizedSet(new HashSet<String>());
-		}
-
-		return result;
-	}
-
 	private static Monitoring load(String id) {
-		before();
-		return (Monitoring) getMemcacheService().get(id);
+		Monitoring monitoring = (Monitoring) getMemcacheService().get(id);
+
+		if (monitoring == null) {
+			Entity entity = loadFromDatastore(id);
+
+			if (entity != null) {
+				monitoring = parse(entity);
+
+				addMonitoringToCache(monitoring);
+				addTrackIdToCache(monitoring.getClientId(), monitoring.getTrackId());
+				addClientIdToCache(monitoring.getClientId());
+			}
+		}
+
+		return monitoring;
 	}
 
 	public static Monitoring load(String clientId, String trackId) {
-		before();
 		return load(getId(clientId, trackId));
-
-		//
-		// Monitoring result = null;
-		//
-		// if (getCachedTrackIds(clientId).contains(trackId)) {
-		// String id = getId(clientId, trackId);
-		// result = (Monitoring) getMemcacheService().get(id);
-		// }
-		//
-		// if (result == null) {
-		// Entity entity = loadFromDatastore(clientId, trackId);
-		// result = parse(entity);
-		//
-		// putOnCache(getId(result));
-		// }
-		//
-		// return result;
 	}
-
-	// private static Entity loadFromDatastore(String clientId, String trackId) {
-	// Query query = new Query(getKind());
-	//
-	// Filter filter = CompositeFilterOperator.and(new FilterPredicate("clientId", Query.FilterOperator.EQUAL,
-	// clientId), new FilterPredicate("trackId", Query.FilterOperator.EQUAL, trackId));
-	// query.setFilter(filter);
-	//
-	// PreparedQuery preparedQuery = getDatastoreService().prepare(query);
-	// return preparedQuery.asSingleEntity();
-
-	// return loadFromDatastore(getId(clientId, trackId));
-	// }
 
 	private static Entity loadFromDatastore(String id) {
 		Entity entity;
@@ -278,10 +335,6 @@ public class MonitoringPersistence {
 		}
 
 		return entity;
-	}
-
-	private static String getId(Entity entity) {
-		return entity.getKey().getName();
 	}
 
 	private static String getId(String clientId, String trackId) {
@@ -296,163 +349,21 @@ public class MonitoringPersistence {
 		return KeyFactory.createKey(getKind(), id);
 	}
 
-	private static void addToCacheQueue(String id) {
-		TaskOptions taskOptions;
-		taskOptions = Builder.withUrl("/admin/monitoring/cache");
-		taskOptions.method(POST);
-
-		if (!Strings.isEmpty(id)) {
-			taskOptions = taskOptions.param("id", id);
-		}
-
-		Queue queue = QueueFactory.getQueue("monitoring-cache");
-		queue.add(taskOptions);
-	}
-
-	@POST
-	public static void addToCache(@FormParam("id") String id) {
-		final String key = "cache_status";
-
-		if (Strings.isEmpty(id)) {
-			getMemcacheService().put(key, "loaded");
-
-		} else {
-			Entity entity = loadFromDatastore(id);
-			Monitoring monitoring = parse(entity);
-
-			Set<String> trackIds = getCachedTrackIds(monitoring.getClientId());
-			trackIds.add(monitoring.getTrackId());
-
-			Set<String> clientIds = getCachedClientIds();
-			clientIds.add(monitoring.getClientId());
-
-			getMemcacheService().put(getKind(), clientIds);
-			getMemcacheService().put(monitoring.getClientId(), trackIds);
-			getMemcacheService().put(id, monitoring);
-		}
-	}
-
-	private static void removeFromCacheQueue(String id) {
-		TaskOptions taskOptions;
-		taskOptions = Builder.withUrl("/admin/monitoring/cache");
-		taskOptions = taskOptions.param("id", id);
-		taskOptions.method(DELETE);
-
-		Queue queue = QueueFactory.getQueue("monitoring-cache");
-		queue.add(taskOptions);
-	}
-
-	// @GET
-	// @Path("/xx")
-	// public static void xxx() {
-	// Query query = new Query(getKind());
-	// PreparedQuery preparedQuery = getDatastoreService().prepare(query);
-	// query.setFilter(new FilterPredicate("clientId", Query.FilterOperator.EQUAL, "91448300404063076307502904506675"));
-	//
-	// for (Entity entity : preparedQuery.asIterable()) {
-	// delete(parse(entity));
-	// }
-	// }
-
-	@DELETE
-	public static void removeFromCache(@QueryParam("id") String id) {
-		Monitoring monitoring = parse(loadFromDatastore(id));
-
-		if (monitoring != null) {
-			String clientId = monitoring.getClientId();
-			String trackId = monitoring.getTrackId();
-
-			Set<String> trackIds = getCachedTrackIds(clientId);
-			trackIds.remove(trackId);
-
-			if (trackIds.isEmpty()) {
-				Set<String> clientIds = getCachedClientIds();
-				clientIds.remove(clientId);
-
-				getMemcacheService().delete(clientId);
-				getMemcacheService().put(getKind(), clientIds);
-
-			} else {
-				getMemcacheService().put(clientId, trackIds);
-			}
-
-			getMemcacheService().delete(getId(monitoring));
-		}
-	}
-
 	public static List<Monitoring> findAll() {
-		before();
 		List<Monitoring> result = new ArrayList<Monitoring>();
 
-		for (String clientId : getCachedClientIds()) {
+		for (String clientId : getClientIds()) {
 			result.addAll(find(clientId));
 		}
 
 		return result;
-
-		// Set<String> clientIds = getCachedClientIds();
-		//
-		// for () {
-		//
-		// }
-
-		// if (clientIds.isEmpty()) {
-		// Monitoring monitoring;
-		//
-		// for (Entity entity : findAllFromDatastore()) {
-		// // TODO Reaproveitar o load!
-		// monitoring = parse(entity);
-		// result.add(monitoring);
-		//
-		// putOnCache(getId(monitoring));
-		// }
-		//
-		// } else {
-		// for (String clientId : clientIds) {
-		// result.addAll(find(clientId));
-		// }
-		// }
-
-		// return result;
 	}
-
-	private static List<String> findAllIdsFromDatastore() {
-		Query query = new Query(getKind()).setKeysOnly();
-		PreparedQuery preparedQuery = getDatastoreService().prepare(query);
-
-		List<String> ids = new ArrayList<String>();
-
-		for (Entity entity : preparedQuery.asIterable()) {
-			ids.add(getId(entity));
-		}
-
-		return ids;
-	}
-
-	// public static List<String> findAllIdsFromDatastore() {
-	// Query query = new Query(getKind()).setKeysOnly();
-	// PreparedQuery preparedQuery = getDatastoreService().prepare(query);
-	//
-	// List<String> ids = Collections.synchronizedList(new ArrayList<String>());
-	//
-	// for (Entity entity : preparedQuery.asIterable()) {
-	// ids.add(entity.getKey().getName());
-	// }
-	//
-	// return ids;
-	// }
 
 	public static List<Monitoring> find(String clientId) {
-		before();
 		List<Monitoring> result = new ArrayList<Monitoring>();
-		Monitoring monitoring;
 
-		for (String trackId : getCachedTrackIds(clientId)) {
-			monitoring = load(clientId, trackId);
-
-			if (monitoring != null) {
-				result.add(load(clientId, trackId));
-			}
+		for (String trackId : getTrackIds(clientId)) {
+			result.add(load(clientId, trackId));
 		}
 
 		return result;
